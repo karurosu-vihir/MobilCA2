2e09884866a68ac8abf6ac352137338fafbb58b5
dry <karurosu.vihir@gmail.com>
2024-07-22 20:01:06 -0600

    primeros cambios y conexion a firebase

--- CA2GrupoH/app/src/main/java/com/example/ca2grupoh/MainActivity.kt
+++ CA2GrupoH/app/src/main/java/com/example/ca2grupoh/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.ca2grupoh
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val db = Firebase.firestore
+        db.collection("libros").get().addOnSuccessListener { result ->
+            for (document in result) {
+                Log.d(TAG, "${document.id} => ${document.data}")
+            }
+            }
+            .addOnFailureListener { exception ->{
+                Log.w(TAG, "Error getting documents.", exception)
+            } }
+        }
     }
-}
